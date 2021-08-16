cacf82a28cd7264ffb5c94320ef47ccb13496336
Liz <thompsonel2011@gmail.com>
2021-08-16 13:12:05 -0400

    resume data

---
+++ resume/src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://thompsonel11.github.io/reactPortfolio/",
+    "name": "Liz Thompson",
+    "role": "Fullstack Developer",
+    "linkedinId":"elizabeth--thompson",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/elizabeth--thompson/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/thompsonel11",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am a Full Stack Developer in training. I currently attend the UNC Chapel Hill Full Stack Developer coding bootcamp.",
+    "address":"Raleigh, NC",
+    "website":"https://thompsonel11.github.io/reactPortfolio/",
+    "education":[
+      {
+        "UniversityName":"The University of North Carolina at Chapel Hill",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2021",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Marist College",
+        "specialization":"COBOL Certification",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"East Carolina University",
+        "specialization":"BS in Secondary Mathematics",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2014",
+        "Achievements":"Magna Cum Laude"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"State Employees Credit Union",
+        "specialization":"Applications Programmer",
+        "MonthOfLeaving":"Current",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"State Employees Credit Union",
+        "specialization":"Business Analyst",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Pour Decisions",
+        "description":"An app that utilizes HTML, Bootstrap, Restful APIs, jQuery, Responsive Design, JavaScript to provide users with a fun, interactive way of choosing their beverage.",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Be | Seated",
+        "description":"An even seating chart app that utilizes HTML, APIs, JavaScript, CSS, MySQL/Sequelize, UUID, Heroke, Multer and Fast-CSV to create an admin and guest experience to simplify the process of creating and distributing a seating chart(s).",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Weather Dashboard",
+        "description":"A clean, simple app that utilizes HTML, CSS, Bootstrap, Handlebars, QR Code and local storage to recall current and 5-day weather forecasts for any US city.",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    ]
+  }
+  
+  export default resumeData
