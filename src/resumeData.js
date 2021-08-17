ea8a65ecf6903228bc7dc92a365ea04da9b3e668
Liz <thompsonel2011@gmail.com>
2021-08-17 11:00:48 -0400

    skills

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,10 +1,10 @@
 let resumeData = {
     "imagebaseurl":"https://thompsonel11.github.io/reactPortfolio/",
     "name": "Liz Thompson",
-    "role": "Fullstack Developer",
+    "role": "Full Stack Developer",
     "linkedinId":" https://www.linkedin.com/in/elizabeth--thompson/",
     // "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": " I love to learn new technologies, create fun, interactive applications and tutor highschool math in my free time. I have worn many hats as a young professional, and have gained useful knowledge and skills with each one of them!",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -64,33 +64,54 @@ let resumeData = {
         // "Achievements":"Some Achievements"
       }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"",
     "skills":[
       {
         "skillname":"HTML5"
       },
       {
-        "skillname":"CSS"
+        "skillname":"CSS, Bootstrap, React"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"Handlebars"
+      }, 
+      {
+        "skillname":"JavaScript, jQuery"
+      },
+      {
+        "skillname":"API, JSON, AJAX"
+      },
+      {
+        "skillname":"Node.JS, Express, User Authentication"
+      },
+      {
+        "skillname":"COBOL, JCL, TSO, ISPF"
+      },
+      {
+        "skillname":"Heroku, GitHub, Git"
+      },
+      {
+        "skillname":"Local Storage, Session Storage, IndexedDB"
+      },
+      {
+        "skillname":"MySQL, MongoDB"
       }
     ],
     "portfolio":[
       {
         "name":"Pour Decisions",
         "description":"An app that utilizes HTML, Bootstrap, Restful APIs, jQuery, Responsive Design, JavaScript to provide users with a fun, interactive way of choosing their beverage.",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"reactPortfolio/public/images/portfolio/modals/m-PourDecisions.png"
       },
       {
         "name":"Be | Seated",
         "description":"An even seating chart app that utilizes HTML, APIs, JavaScript, CSS, MySQL/Sequelize, UUID, Heroke, Multer and Fast-CSV to create an admin and guest experience to simplify the process of creating and distributing a seating chart(s).",
-        "imgurl":"images/portfolio/project.jpg"
+        "imgurl":"reactPortfolio/public/images/portfolio/modals/m-BeSeated.PNG"
       },
       {
         "name":"Weather Dashboard",
         "description":"A clean, simple app that utilizes HTML, CSS, Bootstrap, Handlebars, QR Code and local storage to recall current and 5-day weather forecasts for any US city.",  
-        "imgurl":"images/portfolio/project2.png"
+        "imgurl":"reactPortfolio/public/images/portfolio/modals/m-weatherApp2.png"
       },
     //   {
     //     "name":"project4",
