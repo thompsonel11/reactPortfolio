e66c29beeb85267439a1715a357a98a3389569b0
Liz <thompsonel2011@gmail.com>
2021-08-21 13:47:19 -0400

    Updates

---
+++ precache-manifest.21457927b05f1dde712d54059c832dad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea6315d5a205c189a504",
+    "url": "/reactPortfolio/static/css/main.69205e1d.chunk.css"
+  },
+  {
+    "revision": "ea6315d5a205c189a504",
+    "url": "/reactPortfolio/static/js/main.ea6315d5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/reactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "1eacb2df5595faa617d181a815d58742",
+    "url": "/reactPortfolio/index.html"
+  }
+];
